1e0cec66ea72a14a1c67d1f0a3e657fff215ced4
JiHwan <93183216+JiHwan35@users.noreply.github.com>
2021-12-30 16:31:07 +0900

    First Meeting
    2021-12-30
    First Meeting

--- Game/Assets/Sources/IntroSceneMove.cs
+++ Game/Assets/Sources/IntroSceneMove.cs
@@ -8,7 +8,7 @@ public class IntroSceneMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonUp(0))
         {
             SceneManager.LoadScene("Main");
         }
